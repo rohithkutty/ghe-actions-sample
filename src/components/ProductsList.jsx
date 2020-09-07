5dd386af74479ddffcc89ed52ef22a9b10fa45ac
Rohith Surya Gonela <rohith_gonela@yahoo.com>
2020-09-07 16:37:27 +0530

    updated file name

--- src/components/ProductsList.jsx
+++ src/components/ProductsList.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import Product from './ProductComp';
+import Product from './Product';
 import './ProductsList.styles.css';
 
 const ProductsList = ({ productsList }) => {
--- src/components/ProductsList.test.js
+++ src/components/ProductsList.test.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { shallow } from 'enzyme';
 
 import ProductsList from './ProductsList';
-import Product from './ProductComp';
+import Product from './Product';
 import ProductsData from '../data/db.json';
 
 const { products } = ProductsData;
